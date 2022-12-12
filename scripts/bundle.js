7a3e055690a5a5e1aac5e206c5b2fd7ed552cb23
ElijoaoGarcia <57513172+ElijoaoGarcia@users.noreply.github.com>
2022-12-11 23:00:38 -0400

    scripts/bundle.js file formatter refactor

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,12 +1,12 @@
 
-let sh = require('child_process').execSync;
-let author = sh('git config --get user.email').toString().trim();
-let commits = sh(`git log --author=${ author }`).toString().trim();
+const sh = require('child_process').execSync
+const author = sh('git config --get user.email').toString().trim()
+const commits = sh(`git log --author=${author}`).toString().trim()
 
 if (!commits) {
-  console.error('This repo has no commits from the current Git user. Did you commit your changes?');
-  process.exit(1);
+  console.error('This repo has no commits from the current Git user. Did you commit your changes?')
+  process.exit(1)
 }
 
-let result = sh('git bundle create ICQ24-take-home.bundle HEAD master');
-console.log('Nice work, you created ICQ24-take-home.bundle.');
+sh('git bundle create ICQ24-take-home.bundle HEAD master')
+console.log('Nice work, you created ICQ24-take-home.bundle.')
