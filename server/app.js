33862ec5866a56ff944ce0f3b5fc060892517405
ElijoaoGarcia <57513172+ElijoaoGarcia@users.noreply.github.com>
2022-12-11 22:57:03 -0400

    add agent POST endpoint to add a new agent

--- server/app.js
+++ server/app.js
@@ -2,17 +2,36 @@ const express = require('express')
 const { Agent, Review } = require('./model')
 
 const app = express()
+const router = express.Router()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/agents', async (req, res, next) => {
-  const agents = await Agent.findAll({
-    include: [
-      { model: Review, as: 'reviews' }
-    ]
-  })
-  return res.json(agents)
+const agents = router.route('/agents')
+
+agents.get('/', async (req, res, next) => {
+  try {
+    const agents = await Agent.findAll({
+      include: [
+        { model: Review, as: 'reviews' }
+      ]
+    })
+    return res.json(agents)
+  } catch (error) {
+    res.status(500)
+  }
 })
+
+agents.post('/', async (req, res) => {
+  try {
+    const { agent } = req.body
+    await Agent.create(agent)
+    res.status(200).send()
+  } catch (error) {
+    res.status(500)
+  }
+})
+
+app.use(router)
 
 module.exports = app
