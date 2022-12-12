20bc2cfe770f05b05be4b2b301d67e1fafdc3d74
ElijoaoGarcia <57513172+ElijoaoGarcia@users.noreply.github.com>
2022-12-12 08:52:30 -0400

    add the Review.{css,tsx} files

---
+++ src/components/Agents/Review.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import { IReview } from '../../types/Agent'
+import './Review.css'
+
+interface Props {
+    review: IReview
+}
+
+const Review: FC<Props> = ({ review }) => {
+  return (
+    <div className='review-container'>
+        <h3>{review.fullName}</h3>
+        <p>
+            {review.description}
+        </p>
+    </div>
+  )
+}
+
+export default Review
